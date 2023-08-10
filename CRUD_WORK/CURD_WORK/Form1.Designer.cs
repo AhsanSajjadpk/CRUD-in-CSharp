e501238b3fa3ba298c03f81098286e860959af79
Ahsan Sajjad <princeahsan0608@gmail.com>
2023-08-10 15:44:57 -0700

    Performing All Crud operation in C#

---
+++ CRUD_WORK/CURD_WORK/Form1.Designer.cs
@@ -0,0 +1,237 @@
+﻿
+namespace CURD_WORK
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtid = new System.Windows.Forms.TextBox();
+            this.txtname = new System.Windows.Forms.TextBox();
+            this.txtage = new System.Windows.Forms.TextBox();
+            this.txtgender = new System.Windows.Forms.ComboBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btninsert = new System.Windows.Forms.Button();
+            this.btnupdate = new System.Windows.Forms.Button();
+            this.btndelete = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(257, -4);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(252, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "REGISTRATION FORM";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(91, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(21, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "ID";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(91, 102);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Name";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(91, 140);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(33, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Age";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(91, 173);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(53, 16);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Gender";
+            // 
+            // txtid
+            // 
+            this.txtid.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtid.Location = new System.Drawing.Point(169, 60);
+            this.txtid.Name = "txtid";
+            this.txtid.Size = new System.Drawing.Size(121, 22);
+            this.txtid.TabIndex = 5;
+            this.txtid.Text = "Dont type here";
+            // 
+            // txtname
+            // 
+            this.txtname.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtname.Location = new System.Drawing.Point(169, 98);
+            this.txtname.Name = "txtname";
+            this.txtname.Size = new System.Drawing.Size(121, 22);
+            this.txtname.TabIndex = 6;
+            // 
+            // txtage
+            // 
+            this.txtage.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtage.Location = new System.Drawing.Point(169, 134);
+            this.txtage.Name = "txtage";
+            this.txtage.Size = new System.Drawing.Size(121, 22);
+            this.txtage.TabIndex = 7;
+            // 
+            // txtgender
+            // 
+            this.txtgender.FormattingEnabled = true;
+            this.txtgender.Items.AddRange(new object[] {
+            "MALE",
+            "FEMALE"});
+            this.txtgender.Location = new System.Drawing.Point(169, 167);
+            this.txtgender.Name = "txtgender";
+            this.txtgender.Size = new System.Drawing.Size(121, 21);
+            this.txtgender.TabIndex = 8;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(187, 233);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(453, 187);
+            this.dataGridView1.TabIndex = 9;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // btninsert
+            // 
+            this.btninsert.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btninsert.Location = new System.Drawing.Point(352, 163);
+            this.btninsert.Name = "btninsert";
+            this.btninsert.Size = new System.Drawing.Size(97, 35);
+            this.btninsert.TabIndex = 10;
+            this.btninsert.Text = "INSERT";
+            this.btninsert.UseVisualStyleBackColor = true;
+            this.btninsert.Click += new System.EventHandler(this.btninsert_Click);
+            // 
+            // btnupdate
+            // 
+            this.btnupdate.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnupdate.Location = new System.Drawing.Point(455, 163);
+            this.btnupdate.Name = "btnupdate";
+            this.btnupdate.Size = new System.Drawing.Size(97, 35);
+            this.btnupdate.TabIndex = 11;
+            this.btnupdate.Text = "UPDATE";
+            this.btnupdate.UseVisualStyleBackColor = true;
+            this.btnupdate.Click += new System.EventHandler(this.btnupdate_Click);
+            // 
+            // btndelete
+            // 
+            this.btndelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btndelete.Location = new System.Drawing.Point(558, 163);
+            this.btndelete.Name = "btndelete";
+            this.btndelete.Size = new System.Drawing.Size(97, 35);
+            this.btndelete.TabIndex = 12;
+            this.btndelete.Text = "DELETE";
+            this.btndelete.UseVisualStyleBackColor = true;
+            this.btndelete.Click += new System.EventHandler(this.btndelete_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(661, 163);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(97, 35);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "CLEAR";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btndelete);
+            this.Controls.Add(this.btnupdate);
+            this.Controls.Add(this.btninsert);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.txtgender);
+            this.Controls.Add(this.txtage);
+            this.Controls.Add(this.txtname);
+            this.Controls.Add(this.txtid);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "my APP";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtid;
+        private System.Windows.Forms.TextBox txtname;
+        private System.Windows.Forms.TextBox txtage;
+        private System.Windows.Forms.ComboBox txtgender;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btninsert;
+        private System.Windows.Forms.Button btnupdate;
+        private System.Windows.Forms.Button btndelete;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ CRUD_WORK/CURD_WORK/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace CURD_WORK
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SqlConnection con = new SqlConnection("Data Source=./;Initial Catalog=AHSAN;Integrated Security=True");
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            GetStudentRecord();
+        }
+
+        private void GetStudentRecord()
+        {
+            
+            SqlCommand cmd = new SqlCommand("select * from curd_work",con);
+            DataTable dt = new DataTable();
+
+            con.Open();
+            SqlDataReader sdr = cmd.ExecuteReader();
+            dt.Load(sdr);
+            con.Close();
+            dataGridView1.DataSource = dt;
+
+        }
+
+        private void btninsert_Click(object sender, EventArgs e)
+        {
+            if (isValid())
+            {
+                SqlCommand cmd = new SqlCommand("insert into curd_work values (@name,@age,@gender)", con);
+                cmd.CommandType = CommandType.Text;
+                cmd.Parameters.AddWithValue("@name",txtname.Text);
+                cmd.Parameters.AddWithValue("@age", txtage.Text);
+                cmd.Parameters.AddWithValue("@gender", txtgender.Text);
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+                MessageBox.Show("Insertd", "Student", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                GetStudentRecord();
+                reset();
+            }
+        }
+
+        private bool isValid()
+        {
+            if (txtname.Text==string.Empty)
+            {
+                MessageBox.Show("Insert Name","Fill Student Name",MessageBoxButtons.OK,MessageBoxIcon.Error);
+                return false;
+            }
+            return true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            reset();
+
+        }
+
+        private void reset()
+        {
+            txtname.Clear();
+            txtid.Clear();
+            txtage.Clear();
+            txtname.Focus();
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            txtid.Text = dataGridView1.SelectedRows[0].Cells[0].Value.ToString();
+            txtname.Text = dataGridView1.SelectedRows[0].Cells[1].Value.ToString();
+            txtage.Text = dataGridView1.SelectedRows[0].Cells[2].Value.ToString();
+            txtgender.Text = dataGridView1.SelectedRows[0].Cells[3].Value.ToString();
+        }
+
+        private void btnupdate_Click(object sender, EventArgs e)
+        {
+            if (Convert.ToInt32(txtid.Text)>0)
+            {
+                SqlCommand cmd = new SqlCommand("update curd_work set Name=@name,Age=@age,Gender=@gender where ID=@id", con);
+                cmd.CommandType = CommandType.Text;
+                cmd.Parameters.AddWithValue("@id", Convert.ToInt32(txtid.Text));
+                cmd.Parameters.AddWithValue("@name", txtname.Text);
+                cmd.Parameters.AddWithValue("@age", Convert.ToInt32(txtage.Text));
+                cmd.Parameters.AddWithValue("@gender", txtgender.Text);
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+                MessageBox.Show("Updated", "Student", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                GetStudentRecord();
+                reset();
+            }
+            else
+            {
+                MessageBox.Show("Select data ", "Student", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            
+        }
+
+        private void btndelete_Click(object sender, EventArgs e)
+        {
+            if (Convert.ToInt32(txtid.Text) > 0)
+            {
+                SqlCommand cmd = new SqlCommand("delete from curd_work where ID=@id", con);
+                cmd.CommandType = CommandType.Text;
+                cmd.Parameters.AddWithValue("@id", Convert.ToInt32(txtid.Text));
+                cmd.Parameters.AddWithValue("@name", txtname.Text);
+                cmd.Parameters.AddWithValue("@age", Convert.ToInt32(txtage.Text));
+                cmd.Parameters.AddWithValue("@gender", txtgender.Text);
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+                MessageBox.Show("DELETED", "Student", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                GetStudentRecord();
+                reset();
+            }
+            else
+            {
+                MessageBox.Show("Select data ", "Student", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+
+        }
+    }
+}
